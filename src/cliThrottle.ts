2ee068836dbf3ca570348b182d792bab06a52e2b
igna <126160315+ignasiFloz@users.noreply.github.com>
2023-11-06 10:43:52 +0100

    Update cliThrottle.ts

--- src/cliThrottle.ts
+++ src/cliThrottle.ts
@@ -1,10 +1,31 @@
-// cli.ts
-import  { throttle }  from './throttle';
+import readline from 'readline';
+import { throttle } from './throttle'; // Asegúrate de que el archivo throttle.js esté en la misma carpeta
 
-function ejemplo(args: any[]) {
-  console.log('Función ejecutada con argumentos:', args);
+// Configura la interfaz de línea de comandos para la entrada del usuario
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Importa la función throttle que proporcionaste
+
+const savedValues: string[] = [];
+const saveValueThrottled = throttle((value) => {
+  savedValues.push(value);
+}, 1000);
+
+console.log('Iniciando la grabación. Presiona Enter para almacenar un valor cada segundo. Cuando termines, escribe "F" y presiona Enter.');
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'f') {
+    rl.close();
+    showSavedValues();
+  } else {
+    saveValueThrottled(input);
+  }
+});
+
+function showSavedValues() {
+  console.log('Valores guardados:');
+  console.log(savedValues);
 }
-const throttledFunc = throttle(ejemplo, 5000);
-// Parse command line arguments
-const args = process.argv.slice(2).map((arg) => JSON.parse(arg));
-throttledFunc(args);
